bb64a7ec6fdc9447d48df273422140d38ab7ff66
Priyanka Pujar <110797415+Prii01@users.noreply.github.com>
2024-06-17 21:36:50 +0530

    Update and rename first.java to Patching Array.java

---
+++ Java/Arrays /Patching Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int res = 0;
+        int i=0;
+        while(miss<=n){
+            if(i<nums.length && nums[i]<=miss){
+                miss += nums[i];
+                i++;
+            }else{
+                miss +=miss;
+                res++;
+            }
+        }
+        return res;
+    }
+}
